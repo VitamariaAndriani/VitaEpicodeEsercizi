267a1ef55416db1870841ec134cc60c66aa70539
VitamariaAndriani <135055741+VitamariaAndriani@users.noreply.github.com>
2023-09-05 22:53:10 +0200

    Add files via upload

---
+++ #Esercizio M4_D1.py
@@ -0,0 +1,51 @@
+#Esercizio M4_D1
+
+# Abbiamo 25 studenti; memorizzare questo dato in una variabile
+
+studenti = 25 
+
+# Abbiamo 25 studenti; memorizzare questo dato in una variabile
+# stamparla a video
+
+studenti = 25
+print("studenti", studenti)
+
+#Abbiamo 25 studenti; memorizzare questo dato in una variabile
+#Arrivano altri 3 studenti; memorizzare questo dato in un altra variabile
+
+studenti = 25
+atri_studenti = 3
+
+#Abbiamo 25 studenti; memorizzare questo dato in una variabile
+#Arrivano altri 3 studenti; memorizzare questo dato in un altra variabile
+#Creare un altra variabile ancora che conterrà, la somma delle prime due, poi stamparle a video
+
+studenti = 25
+altri_studenti = 3
+somma_studenti = studenti + altri_studenti
+print(somma_studenti)
+
+#Creare a mano, una lista che contenga i numeri che va da 0 a 5
+#Memorizzare in una variabile e stamparla a video
+
+lista_numeri =[0, 1, 2, 3, 4, 5]
+print("la lista è questa:",lista_numeri)  
+
+#Creare a mano una lista, che contenga tre titoli dei tuoi film preferiti
+#Memorizzare in una variabile e stamparla a video
+
+lista_film = ["Tre metri sopra il cielo", 
+              "La mummia", 
+              "Harry Potter"]
+print("la lista è questa:",lista_film)
+
+
+
+
+
+
+
+
+
+#
+
